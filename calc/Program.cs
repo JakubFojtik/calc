c0a4fbefd93818c2e8c1fc83b247428ae70c0163
Jakub Fojtík <jakub.fojtik@email.cz>
2017-07-10 01:28:28 +0200

    lexer

---
+++ calc/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace calc
+{
+    class Program
+    {
+        static char[] decSeps = { ',', '.' };
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Priklad: ");
+                string input = Console.ReadLine();
+
+                List<Token> tokens = lexer(input);
+                Console.WriteLine(string.Join(", ", tokens));
+
+                Console.WriteLine();
+                AST ast = parser(tokens);
+                printAST(ast);
+            }
+        }
+
+        private static void printAST(AST ast)
+        {
+            Queue<AST> q = new Queue<AST>();
+            q.Enqueue(ast);
+            int numItems = 1;
+            while (q.Count > 0)
+            {
+                AST item = q.Dequeue();
+                Console.Write(item.value + " ");
+                numItems--;
+                if (numItems == 0) Console.WriteLine();
+                AST nextItem = item.left;
+                if (nextItem != null)
+                {
+                    q.Enqueue(nextItem);
+                    numItems++;
+                }
+                nextItem = item.right;
+                if (nextItem != null)
+                {
+                    q.Enqueue(nextItem);
+                    numItems++;
+                }
+            }
+        }
+
+        private static AST parser(List<Token> tokens)
+        {
+            return new AST(new Token(TokenType.Operator, OperatorType.Plus),
+                new AST(new Token(TokenType.Number, 5)),
+                new AST(new Token(TokenType.Number, -1)));
+        }
+
+        static bool isNumber(string buffer)
+        {
+            return !string.IsNullOrWhiteSpace(buffer) && !buffer.Select(x => char.IsDigit(x) || decSeps.Contains(x)).Contains(false);
+            //     && buffer.Count(x => decSeps.Contains(x)) <= 1;   aby ... nebylo 0 0 0 tak je tecka "cislici"-soucasti cisla
+        }
+
+        private static bool isOperatorPrefix(string buffer)
+        {
+            //Prefix enumeration of all operators. Return if it contains the buffer.
+            return !string.IsNullOrWhiteSpace(buffer)
+                && operators.Keys.SelectMany(x => Enumerable.Range(0, x.Length + 1).Select(i => x.Substring(0, i))).Contains(buffer);
+        }
+
+        static Dictionary<string, Tuple<TokenType, object>> operators = new Dictionary<string, Tuple<TokenType, object>>
+        {
+            { "(", new Tuple<TokenType, object>(TokenType.BraceOpen, null)},
+            { ")", new Tuple<TokenType, object>(TokenType.BraceClose, null)},
+            { "+", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.Plus)},
+            { "-", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.Minus)},
+            { "*", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.Star)},
+            { "/", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.Slash)},
+            { "sin", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.Sin)},
+            { "sinh", new Tuple<TokenType, object>(TokenType.Operator, OperatorType.SinH)},
+        };
+
+        public enum State { Empty, Number, Operator }
+
+        private static List<Token> lexer(string input)
+        {
+            List<Token> tokens = new List<Token>();
+            string buffer = "";
+            State state = State.Empty;
+            Action setEmpty = () => { buffer = ""; state = State.Empty; };
+            Action flushBuffer = () =>
+            {
+                if (string.IsNullOrWhiteSpace(buffer)) return;
+                if (isNumber(buffer))
+                {
+                    if (buffer.Count(x => decSeps.Contains(x)) > 1) throw new InvalidOperationException("multiple decseps");
+                    if (decSeps.Contains(buffer.First())) buffer = 0 + buffer;
+                    if (decSeps.Contains(buffer.Last())) buffer = buffer.TrimEnd(decSeps);
+                    buffer = string.Join(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, buffer.Split(decSeps));
+                    tokens.Add(new Token(TokenType.Number, Convert.ToDouble(buffer)));
+                }
+                else if (operators.ContainsKey(buffer)) tokens.Add(new Token(operators[buffer].Item1, operators[buffer].Item2));
+                else throw new InvalidOperationException("badbuffer");
+            };
+
+            //Action<TokenType, object> addToken = (type, val) => tokens.Add(new Token(type, val));
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                switch (state)
+                {
+                    case State.Empty:
+                        buffer += c;
+                        state = isNumber(buffer) ? State.Number : State.Operator;
+                        break;
+                    case State.Number:
+                        if (isNumber(buffer + c)) buffer += c;
+                        else
+                        {
+                            flushBuffer();
+                            i--;
+                            setEmpty();
+                        }
+                        break;
+                    case State.Operator:
+                        if (isOperatorPrefix(buffer + c)) buffer += c;
+                        else
+                        {
+                            flushBuffer();
+                            i--;
+                            setEmpty();
+                        }
+                        break;
+                }
+            }
+            flushBuffer();
+
+            return tokens;
+        }
+
+        public enum TokenType { BraceOpen, BraceClose, Number, Operator }
+        public enum OperatorType { Plus, Minus, Star, Slash, Sin, SinH }
+
+        public class Token
+        {
+            public TokenType Type { get; set; }
+            public object Value { get; set; }
+
+            public Token(TokenType type, object value = null)
+            {
+                Type = type;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}({1})", Type, Value);
+            }
+        }
+
+        public class AST
+        {
+            public Token value;
+            public AST left;
+            public AST right;
+
+            public AST(Token v, AST l, AST r)
+            {
+                value = v;
+                left = l;
+                right = r;
+            }
+            public AST(Token v) : this(v, null, null) { }
+        }
+
+    }
+}
