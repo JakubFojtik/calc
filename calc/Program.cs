d9c781c53f3102f79165d5a3478e7b9952bd76d2
Jakub Fojtik <jakub.fojtik@email.cz>
2018-08-11 02:04:43 +0200

    clean+fix

--- calc/Program.cs
+++ calc/Program.cs
@@ -52,7 +52,15 @@ namespace calc
         }
 
         private static List<Token> tokens;
-        private static Token curTok;
+        private static int curTokIdx;
+        private static Token curTok
+        {
+            get
+            {
+                if (curTokIdx < tokens.Count) return tokens[curTokIdx];
+                else return new Token(TokenType.EOF);
+            }
+        }
 
         private static void stackParser(List<Token> tokens)
         {
@@ -67,7 +75,7 @@ namespace calc
             });
 
         }
-        
+
         private static bool tryReduce(Stack<AST> stack, Token x)
         {
             //2 aargs for binops, 1 for unop, braces
@@ -80,10 +88,10 @@ namespace calc
 
             return false;
         }
-        
+
         private static void recWork(int start, Priority currentPriority)
         {
-            //5+4-6*9^8-4
+            //5+4-6*9^8*2-4
             //readterm .5 =>5.
             //reaadop + pri=add
             //readexpr(untilAddOrBigger) =>.4-
@@ -109,8 +117,8 @@ namespace calc
             parse(all)
                 while(cantstop) read
                 ast token v tokenech
-                na zaac zav unarni +- bez priority
-                co s sin 5 + 2
+                na zac zav unarni +- bez priority
+                co s sin 5 + 2 - zrat minimum
                 recwork(sezer mi 1 argument jsem sinus)
                 -5^4 - co driv? ^ neresim unarni minus
                 pada priorita, zavorky jsou samost
@@ -118,60 +126,132 @@ namespace calc
 
                 priority - podle googlu sin sin 2 ^ 3 ^ 4 = sin(sin(2^(3^4)))
                 tj 2+2+2 = 2+(2+2) tj ctu aalespon stejne priority
+                ale 1/2/3 = (1/2)/3
+                takze dulezitejsi  je poradi vyhodnocovani
             */
 
-            curTok = tokens.First();
-            recdesc();
+            curTokIdx = 0;
+            Console.WriteLine(readAll());
 
             return new AST(new Token(TokenType.Operator, OperatorType.Plus),
                 new AST(new Token(TokenType.Number, 5)),
                 new AST(new Token(TokenType.Number, -1)));
         }
 
-        private static void recdesc()
+        class Value
         {
-            //expr
-            //readfirst
-            //readtok
-            //readsec:
-            //posilat startIndex
-            if(curTok.Type == TokenType.Operator)
+            private decimal val;
+
+            public Value(decimal d)
+            {
+                val = d;
+            }
+
+            public static implicit operator Value(decimal d)
+            {
+                return new Value(d);
+            }
+
+            public void Negate()    // Invert?
+            {
+                val = -val;
+            }
+
+            public override string ToString()
+            {
+                return val.ToString();
+            }
+        }
+
+
+        //Gramatika - nejvic zanorene jsou nejvyssi priority, Start: SCIT
+        //SCIT -> SCIT + NAS | NAS
+        //NAS -> NAS * MOC | MOC
+        //MOC -> MOC * CISLO | CISLO
+        //CISLO -> CISLO | (SCIT)
+        //odstr leve rekurze SCIT->NAS ?SCIT, ?SCIT->+ NAS ?SCIT|eps
+        //stromecek
+        private static Value readAll()
+        {
+            Value retExp;
+            Value retRem;
+            retExp = readRem();
+            retRem = readRem();
+            return retExp; //todo?
+        }
+
+        private static Value readRem() //??? read scit aa read nas...
+        {
+            Value ret;
+            if (curTok.Type == TokenType.Operator)
             {
                 var op = (OperatorType)curTok.Value;
                 if (op == OperatorType.Plus || op == OperatorType.Minus)
-                     readFactor();//return
+                    ret = readFactor();//return
                 else throw new ArithmeticException(ERROR);
             }
-            if (curTok.Type == TokenType.BraceOpen)
-                 readFactor();//return
+            else if (curTok.Type == TokenType.Number)
+                ret = readFactor();
+            else if (curTok.Type == TokenType.BraceOpen)
+                ret = readBrace();
+            else throw new ArithmeticException(ERROR);
+            return ret;
         }
 
-        private static void readFactor()
+        private static Value readFactor()
         {
+            Value ret;
             bool signIsPlus = true;
-            while(curTok.Type==TokenType.Operator)
+            while (curTok.Type == TokenType.Operator)
             {
                 var op = (OperatorType)curTok.Value;
                 if (op == OperatorType.Minus) signIsPlus ^= true;
                 else if (op == OperatorType.Plus) { }
                 else throw new ArithmeticException(ERROR);
+                curTokIdx++;
             }
+
             if (curTok.Type == TokenType.Number)
             {
-                var num = (decimal)curTok.Value;
-                if (!signIsPlus) num = -num;
-                //nejakvratit
+                ret = Convert.ToDecimal(curTok.Value);
+                curTokIdx++;
             }
-            if (curTok.Type == TokenType.BraceOpen)
+            else if (curTok.Type == TokenType.BraceOpen)
             {
-                //nejak++ a readFactor()
+                ret = readBrace();
             }
+            else throw new ArithmeticException(ERROR);
+
+            if (!signIsPlus) ret.Negate();
+            return ret;
+        }
+
+        private static Value readBrace()
+        {
+            Value ret;
+            curTokIdx++;
+            ret = readRem();
+            if (curTok.Type == TokenType.BraceClose)
+            {
+                curTokIdx++;
+            }
+            else throw new ArithmeticException(ERROR);
+            return ret;
         }
 
         static bool isNumeric(string buffer)
         {
-            return !string.IsNullOrWhiteSpace(buffer) && !buffer.Select(x => char.IsDigit(x) || decSeps.Contains(x)).Contains(false);
+            return !string.IsNullOrWhiteSpace(buffer) && areAllCharsNumeric(buffer);
             //     && buffer.Count(x => decSeps.Contains(x)) <= 1;   aby ... nebylo 0 0 0 tak je tecka "cislici"-soucasti cisla
+        }
+
+        private static bool areAllCharsNumeric(string buffer)
+        {
+            foreach (var c in buffer)
+            {
+                if (!char.IsDigit(c) && !decSeps.Contains(c)) return false;
+            }
+            return true;
         }
 
         private static bool isOperatorPrefix(string buffer)
@@ -179,24 +259,26 @@ namespace calc
             return !string.IsNullOrWhiteSpace(buffer) && operatorPrefixes.Contains(buffer);
         }
 
-        enum Priority { None = 0, Add, Mult, Pow, Unary, Brace } //brace mimo
+        enum Priority { None = 0, Add, Mult, Unary, Pow, Brace } //brace mimo?
 
-        class OperatorData : Tuple<TokenType, OperatorType, Priority>
+        enum Associativity { Left = 0, Right }
+
+        class OperatorData : Tuple<TokenType, OperatorType, Priority, Associativity>
         {
-            public OperatorData(TokenType item1, OperatorType item2, Priority item3) : base(item1, item2, item3) { }
+            public OperatorData(TokenType item1, OperatorType item2, Priority item3, Associativity item4) : base(item1, item2, item3, item4) { }
         }
 
         static Dictionary<string, OperatorData> operators = new Dictionary<string, OperatorData>
         {
-            { "(",    new OperatorData(TokenType.BraceOpen, OperatorType.None , Priority.Brace)},
-            { ")",    new OperatorData(TokenType.BraceClose, OperatorType.None, Priority.Brace)},
-            { "+",    new OperatorData(TokenType.Operator, OperatorType.Plus ,Priority.Add)},
-            { "-",    new OperatorData(TokenType.Operator, OperatorType.Minus,Priority.Add)},   //dynamic
-            { "*",    new OperatorData(TokenType.Operator, OperatorType.Star ,Priority.Mult)},
-            { "/",    new OperatorData(TokenType.Operator, OperatorType.Slash,Priority.Mult)},
-            { "^",    new OperatorData(TokenType.Operator, OperatorType.Caret ,Priority.Pow)},
-            { "sin",  new OperatorData(TokenType.Operator, OperatorType.Sin  ,Priority.Unary)},
-            { "sinh", new OperatorData(TokenType.Operator, OperatorType.SinH ,Priority.Unary)},
+            { "(",    new OperatorData(TokenType.BraceOpen,  OperatorType.None,  Priority.Brace, Associativity.Right )},
+            { ")",    new OperatorData(TokenType.BraceClose, OperatorType.None,  Priority.Brace, Associativity.Left  )},
+            { "+",    new OperatorData(TokenType.Operator,   OperatorType.Plus,  Priority.Add,   Associativity.Left  )},
+            { "-",    new OperatorData(TokenType.Operator,   OperatorType.Minus, Priority.Add,   Associativity.Left  )}, //dynamic
+            { "*",    new OperatorData(TokenType.Operator,   OperatorType.Star,  Priority.Mult,  Associativity.Left  )},
+            { "/",    new OperatorData(TokenType.Operator,   OperatorType.Slash, Priority.Mult,  Associativity.Left  )},
+            { "^",    new OperatorData(TokenType.Operator,   OperatorType.Caret, Priority.Pow,   Associativity.Right )},
+            { "sin",  new OperatorData(TokenType.Operator,   OperatorType.Sin ,  Priority.Unary, Associativity.Left  )},
+            { "sinh", new OperatorData(TokenType.Operator,   OperatorType.SinH,  Priority.Unary, Associativity.Left  )},
         };
         //Prefix enumeration of all operators.
         static string[] _operatorPrefixes;
@@ -206,8 +288,11 @@ namespace calc
         {
             get
             {
+                Func<string, IEnumerable<int>> substrLengths = str => Enumerable.Range(1, str.Length);
+                Func<string, IEnumerable<int>, IEnumerable<string>> substrings = (str, lengths) => lengths.Select(i => str.Substring(0, i));
+
                 return _operatorPrefixes
-                    ?? (_operatorPrefixes = operators.Keys.SelectMany(x => Enumerable.Range(0, x.Length + 1).Select(i => x.Substring(0, i))).ToArray());
+                    ?? (_operatorPrefixes = operators.Keys.SelectMany(x => substrings(x, substrLengths(x))).ToArray());
             }
         }
 
@@ -218,8 +303,8 @@ namespace calc
             List<Token> tokens = new List<Token>();
             string buffer = "";
             State state = State.Empty;
-            Action setEmpty = () => { buffer = ""; state = State.Empty; };
-            Action flushBuffer = () =>
+            //Action<TokenType, object> addToken = (type, val) => tokens.Add(new Token(type, val));
+            Action finalizeBuffer = () =>
             {
                 if (string.IsNullOrWhiteSpace(buffer)) return;
                 if (isNumeric(buffer))
@@ -227,14 +312,17 @@ namespace calc
                     if (buffer.Count(x => decSeps.Contains(x)) > 1) throw new InvalidOperationException("multiple decseps");
                     if (decSeps.Contains(buffer.First())) buffer = 0 + buffer;
                     if (decSeps.Contains(buffer.Last())) buffer = buffer.TrimEnd(decSeps);
+                    // convert all decimal separators to one - does not support thousand separators
                     buffer = string.Join(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator, buffer.Split(decSeps));
                     tokens.Add(new Token(TokenType.Number, Convert.ToDouble(buffer)));
                 }
                 else if (operators.ContainsKey(buffer)) tokens.Add(new Token(operators[buffer].Item1, operators[buffer].Item2));
                 else throw new InvalidOperationException("badbuffer");
+
+                Action setEmpty = () => { buffer = ""; state = State.Empty; };
+                setEmpty();
             };
 
-            //Action<TokenType, object> addToken = (type, val) => tokens.Add(new Token(type, val));
             for (int i = 0; i < input.Length; i++)
             {
                 char c = input[i];
@@ -248,28 +336,26 @@ namespace calc
                         if (isNumeric(buffer + c)) buffer += c;
                         else
                         {
-                            flushBuffer();
+                            finalizeBuffer();
                             i--;
-                            setEmpty();
                         }
                         break;
                     case State.Operator:
                         if (isOperatorPrefix(buffer + c)) buffer += c;
                         else
                         {
-                            flushBuffer();
+                            finalizeBuffer();
                             i--;
-                            setEmpty();
                         }
                         break;
                 }
             }
-            flushBuffer();
+            finalizeBuffer();
 
             return tokens;
         }
 
-        public enum TokenType { BraceOpen, BraceClose, Number, Operator }
+        public enum TokenType { BraceOpen, BraceClose, Number, Operator, EOF }
         public enum OperatorType { None, Plus, Minus, Star, Slash, Sin, SinH, Caret }
 
         public class Token
@@ -293,7 +379,7 @@ namespace calc
         {
             public AST Ast { get; set; }
 
-            public ASTToken(AST ast) : base(TokenType.Number)
+            public ASTToken(AST ast) : base(TokenType.Number)   //TokenType.Operator?
             {
                 Ast = ast;
             }
